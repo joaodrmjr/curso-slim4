7b273280620b6a4766011dff967d40e6e13fd331
= <joaodrmjr@gmail.com>
2024-02-19 08:16:25 -0300

    Slim 4 Setup , ola mundo

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+
+require __DIR__ . "/../vendor/autoload.php";
+
+
+
+$app = AppFactory::create();
+
+
+
+$app->get("/", function (Request $request, Response $response, $args) {
+	$response->getBody()->write("Ola Mundo!");
+	return $response;
+});
+
+
+
+$app->run();
