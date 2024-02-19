d368aa3ad708e5c254ac9d6aa507e0e9b1661cca
= <joaodrmjr@gmail.com>
2024-02-19 08:38:31 -0300

    Slim twig view

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,8 @@ use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use DI\Container;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 
 
@@ -15,13 +17,22 @@ require __DIR__ . "/../vendor/autoload.php";
 $container = new Container();
 
 AppFactory::setContainer($container);
+
+
+$container->set("view", function ($container) {
+	return Twig::create(__DIR__ . "/../resources/views", ["cache" => __DIR__ . "/../cache/twig"]);
+});
+
+
 $app = AppFactory::create();
+
+
+$app->add(TwigMiddleware::createFromContainer($app));
 
 
 $app->addErrorMiddleware(true, false, false);
 
 
 $app->get("/", function (Request $request, Response $response, $args) {
-	$response->getBody()->write("Ola Mundo!");
-	return $response;
+	return $this->get("view")->render($response, "home.twig");
 });
